8d93b815c4734cfcb6da0c415823f5960d39cc13
cheesypoof <cheesypoof@users.noreply.github.com>
2014-01-29 11:56:31 -0800

    Drop fieldset

--- account.php
+++ account.php
@@ -15,124 +15,120 @@
 	</cms:if>
 
 	<cms:pages id=my_user_id limit='1' masterpage='users.php'>
-		<fieldset>
-			<legend>Information</legend>
+		<h2>Information</h2>
 
-			<cms:form anchor='0' action="<cms:link 'account.php'/>" masterpage='users.php' method='post' mode='edit' name='information' page_id=my_user_id>
-				<cms:if k_success>
-					<cms:embed 'user-edit-information.php'/>
+		<cms:form anchor='0' action="<cms:link 'account.php'/>" masterpage='users.php' method='post' mode='edit' name='information' page_id=my_user_id>
+			<cms:if k_success>
+				<cms:embed 'user-edit-information.php'/>
+			<cms:else/>
+				<cms:if k_error>
+					<div class="alert alert-error">
+						<cms:each k_error>
+							<cms:show item/><br/>
+						</cms:each>
+					</div>
 				<cms:else/>
-					<cms:if k_error>
-						<div class="alert alert-error">
-							<cms:each k_error>
-								<cms:show item/><br/>
-							</cms:each>
-						</div>
-					<cms:else/>
-						<cms:get_flash 'information_success'/>
-					</cms:if>
+					<cms:get_flash 'information_success'/>
 				</cms:if>
+			</cms:if>
 
-				<div><label for="f_user_fname">First Name</label></div>
-				<div><cms:input name='user_fname' type='bound'/></div>
+			<div><label for="f_user_fname">First Name</label></div>
+			<div><cms:input name='user_fname' type='bound'/></div>
 
-				<br/>
+			<br/>
 
-				<div><label for="f_user_lname">Last Name</label></div>
-				<div><cms:input name='user_lname' type='bound'/></div>
+			<div><label for="f_user_lname">Last Name</label></div>
+			<div><cms:input name='user_lname' type='bound'/></div>
 
-				<br/>
+			<br/>
 
-				<input type="submit" value="Update Information"/>
-			</cms:form>
-		</fieldset>
+			<input type="submit" value="Update Information"/>
+		</cms:form>
 
 		<br/>
 
-		<fieldset>
-			<legend>Account</legend>
+		<h2>Account</h2>
 
-			<cms:form anchor='0' action="<cms:link 'account.php'/>" method='post' name='account'>
-				<cms:if k_success>
-					<cms:embed 'user-password-verify.php'/>
+		<cms:form anchor='0' action="<cms:link 'account.php'/>" method='post' name='account'>
+			<cms:if k_success>
+				<cms:embed 'user-password-verify.php'/>
 
-					<cms:if password_verified>
-						<cms:embed 'user-exists.php'/>
+				<cms:if password_verified>
+					<cms:embed 'user-exists.php'/>
 
-						<cms:if continue_user_name && continue_email>
-							<cms:if "<cms:not_empty frm_new_password/>" && "<cms:not_empty frm_new_password_repeat/>">
-								<cms:embed 'user-password-new.php'/>
-							</cms:if>
-
-							<cms:if new_user_name || new_email || "<cms:not_empty new_password_hash/>">
-								<cms:embed 'user-edit-account.php'/>
-							<cms:else/>
-								<div class="alert">You didn't enter any new account settings.</div>
-							</cms:if>
+					<cms:if continue_user_name && continue_email>
+						<cms:if "<cms:not_empty frm_new_password/>" && "<cms:not_empty frm_new_password_repeat/>">
+							<cms:embed 'user-password-new.php'/>
 						</cms:if>
-					<cms:else/>
-						<div class="alert alert-error">Incorrect password. Please try again.</div>
+
+						<cms:if new_user_name || new_email || "<cms:not_empty new_password_hash/>">
+							<cms:embed 'user-edit-account.php'/>
+						<cms:else/>
+							<div class="alert">You didn't enter any new account settings.</div>
+						</cms:if>
 					</cms:if>
 				<cms:else/>
-					<cms:if k_error>
-						<div class="alert alert-error">
-							<cms:each k_error>
-								<cms:show item/><br/>
-							</cms:each>
-						</div>
-					<cms:else/>
-						<cms:get_flash 'account_success'/>
-					</cms:if>
+					<div class="alert alert-error">Incorrect password. Please try again.</div>
 				</cms:if>
+			<cms:else/>
+				<cms:if k_error>
+					<div class="alert alert-error">
+						<cms:each k_error>
+							<cms:show item/><br/>
+						</cms:each>
+					</div>
+				<cms:else/>
+					<cms:get_flash 'account_success'/>
+				</cms:if>
+			</cms:if>
 
-				<div><label for="password">Current Password (Required)</label></div>
-				<div><input autocomplete="off" id="password" maxlength="64" name="password" pattern=".{6,64}" required="required" title="6 to 64 characters" type="password" value=""/></div>
+			<div><label for="password">Current Password (Required)</label></div>
+			<div><input autocomplete="off" id="password" maxlength="64" name="password" pattern=".{6,64}" required="required" title="6 to 64 characters" type="password" value=""/></div>
 
-				<cms:hide>
-					<cms:input label='Current Password' name='password' required='1' type='password' validator='min_len=6 | max_len=64'/>
-				</cms:hide>
+			<cms:hide>
+				<cms:input label='Current Password' name='password' required='1' type='password' validator='min_len=6 | max_len=64'/>
+			</cms:hide>
 
-				<br/>
+			<br/>
 
-				<div><label for="user_name">Username (Required)</label></div>
-				<div><input id="user_name" maxlength="48" name="user_name" pattern="[a-zA-Z0-9]{3,48}" required="required" title="3 to 48 alphanumeric characters" type="text" value="<cms:show user_name/>"/></div>
+			<div><label for="user_name">Username (Required)</label></div>
+			<div><input id="user_name" maxlength="48" name="user_name" pattern="[a-zA-Z0-9]{3,48}" required="required" title="3 to 48 alphanumeric characters" type="text" value="<cms:show user_name/>"/></div>
 
-				<cms:hide>
-					<cms:input label='Username' name='user_name' required='1' type='text' validator='alpha_num | min_len=3 | max_len=48'/>
-				</cms:hide>
+			<cms:hide>
+				<cms:input label='Username' name='user_name' required='1' type='text' validator='alpha_num | min_len=3 | max_len=48'/>
+			</cms:hide>
 
-				<br/>
+			<br/>
 
-				<div><label for="email">Email Address (Required)</label></div>
-				<div><input id="email" name="email" required="required" type="email" value="<cms:show user_email/>"/></div>
+			<div><label for="email">Email Address (Required)</label></div>
+			<div><input id="email" name="email" required="required" type="email" value="<cms:show user_email/>"/></div>
 
-				<cms:hide>
-					<cms:input label='Email Address' name='email' required='1' type='text' validator='email'/>
-				</cms:hide>
+			<cms:hide>
+				<cms:input label='Email Address' name='email' required='1' type='text' validator='email'/>
+			</cms:hide>
 
-				<br/>
+			<br/>
 
-				<div><label for="new_password">New Password</label></div>
-				<div><input autocomplete="off" class="span6" id="new_password" maxlength="64" name="new_password" type="password" value=""/></div>
+			<div><label for="new_password">New Password</label></div>
+			<div><input autocomplete="off" class="span6" id="new_password" maxlength="64" name="new_password" type="password" value=""/></div>
 
-				<cms:hide>
-					<cms:input label='New Password' name='new_password' type='password' validator='min_len=6 | max_len=64 | matches_field=new_password_repeat'/>
-				</cms:hide>
+			<cms:hide>
+				<cms:input label='New Password' name='new_password' type='password' validator='min_len=6 | max_len=64 | matches_field=new_password_repeat'/>
+			</cms:hide>
 
-				<br/>
+			<br/>
 
-				<div><label for="new_password_repeat">Repeat New Password</label></div>
-				<div><input autocomplete="off" class="span6" id="new_password_repeat" maxlength="64" name="new_password_repeat" type="password" value=""/></div>
+			<div><label for="new_password_repeat">Repeat New Password</label></div>
+			<div><input autocomplete="off" class="span6" id="new_password_repeat" maxlength="64" name="new_password_repeat" type="password" value=""/></div>
 
-				<cms:hide>
-					<cms:input label='Repeat New Password' name='new_password_repeat' type='password' validator='min_len=6 | max_len=64 | matches_field=new_password'/>
-				</cms:hide>
+			<cms:hide>
+				<cms:input label='Repeat New Password' name='new_password_repeat' type='password' validator='min_len=6 | max_len=64 | matches_field=new_password'/>
+			</cms:hide>
 
-				<br/>
+			<br/>
 
-				<input type="submit" value="Update Account"/>
-			</cms:form>
-		</fieldset>
+			<input type="submit" value="Update Account"/>
+		</cms:form>
 	</cms:pages>
 <cms:else/>
 	<cms:if get_activate>
