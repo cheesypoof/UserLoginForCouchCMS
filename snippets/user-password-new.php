2c8d21a4b7603c0e17628222ce52adf2df263d97
cheesypoof <cheesypoof@users.noreply.github.com>
2014-02-07 22:56:48 -0800

    Password hash argument consistency

--- snippets/user-password-new.php
+++ snippets/user-password-new.php
@@ -1,7 +1,7 @@
 <cms:php>
-global $CTX, $FUNCS;
+global $CTX;
 
-$user_password_hash = password_hash( $CTX->get( 'frm_new_password' ), PASSWORD_DEFAULT, array( 'cost' => USER_PASSWORD_HASH_COST_FACTOR ) );
+$user_password_hash = password_hash( $_POST['new_password'], PASSWORD_DEFAULT, array( 'cost' => USER_PASSWORD_HASH_COST_FACTOR ) );
 
 $CTX->set( 'new_password_hash', $user_password_hash );
 </cms:php>
