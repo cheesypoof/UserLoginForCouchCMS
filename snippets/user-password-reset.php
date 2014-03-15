36ef0493aa8485153b0fbe1fd516aec6c1fbdedc
cheesypoof <cheesypoof@users.noreply.github.com>
2014-03-14 20:01:32 -0700

    Relocate password reset function

--- snippets/user-password-reset.php
+++ snippets/user-password-reset.php
@@ -15,7 +15,7 @@ $CTX->set( 'password_reset_hash', sha1( uniqid( mt_rand(), true ) ) );
 
 <cms:send_mail from=k_email_from to=user_email subject='Password Reset for Website Name'>Hello <cms:show user_name/>,
 
-Please visit this link to reset your account password: <cms:link 'reset.php'/>?id=<cms:show k_page_id/>&amp;hash=<cms:show password_reset_hash/>
+Please visit this link to reset your account password: <cms:link 'reset.php'/>?act=reset&amp;id=<cms:show k_page_id/>&amp;hash=<cms:show password_reset_hash/>
 
 Thanks,
 Website Name</cms:send_mail>
